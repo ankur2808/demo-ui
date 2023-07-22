50755c1933973b643b720a006ffd175c7c3fb1d6
ankur2808 <ankur.verma2808@gmail.com>
2023-07-22 14:04:19 +0000

    update

---
+++ src/hello/hello.ts
@@ -0,0 +1,3 @@
+export const helloWorld=()=>{
+    console.log("hí3333")
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { helloWorld } from "./hello/hello";
+
+exports.handler = async (event:any) => {
+  // TODO implement
+  helloWorld();
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Ankur!'),
+  };
+  return response;
+};
